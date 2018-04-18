e562f22af960aa80982d130247c114d59bc85f33
Adrian Tineo <adrian@adriantineo.com>
2018-04-18 11:14:19 +0700

    Adapt make model part (NOK)

--- TopQuestion/APIResources.swift
+++ TopQuestion/APIResources.swift
@@ -8,24 +8,10 @@
 
 import Foundation
 
-struct ApiWrapper {
-	let items: [Serialization]
-}
-
-extension ApiWrapper {
-	private enum Keys: String, SerializationKey {
-		case items
-	}
-	
-	init(serialization: Serialization) {
-		items = serialization.value(forKey: Keys.items) ?? []
-	}
-}
-
 protocol ApiResource {
-	associatedtype Model
+    associatedtype Model: Decodable
 	var methodPath: String { get }
-	func makeModel(serialization: Serialization) -> Model
+    func makeModel(data: Data) -> [Model]?
 }
 
 extension ApiResource {
@@ -38,23 +24,25 @@ extension ApiResource {
 		let url = baseUrl + methodPath + "?" + order + "&" + sorting + "&" + tags + "&" + site
 		return URL(string: url)!
 	}
-	
-	func makeModel(data: Data) -> [Model]? {
-		guard let json = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) else {
-			return nil
-		}
-		guard let jsonSerialization = json as? Serialization else {
-			return nil
-		}
-		let wrapper = ApiWrapper(serialization: jsonSerialization)
-		return wrapper.items.map(makeModel(serialization:))
+
+    func makeModel(data: Data) -> [Model]? {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        
+        do {
+            let models = try decoder.decode(Array<Model>.self, from: data)
+            print(models)
+            return models
+        } catch let error {
+            print(error)
+            return nil
+        }
 	}
 }
 
 struct QuestionsResource: ApiResource {
+    typealias Model = Question
+    
 	let methodPath = "/questions"
-	
-	func makeModel(serialization: Serialization) -> Question {
-		return Question(serialization: serialization)
-	}
+    
 }
